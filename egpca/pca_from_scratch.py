57fb520df44b8c45a01d25aaffd9d0d246bbc950
davide gambocci <dav.gambocci@gmail.com>
2020-11-16 22:44:18 +0100

    Aggiunta PCA

--- egpca/pca_from_scratch.py
+++ egpca/pca_from_scratch.py
@@ -1,14 +1,26 @@
 import numpy as np
+from typing import List
+
 
 np.random.seed(123)
 
-
-
+#
 def de_mean_matrix(A: np.ndarray) -> np.ndarray:
+    """returns the result of subtracting from every value in A the mean
+    value of its column. the resulting matrix has mean 0 in every column"""
     return A - A.mean(axis=0, keepdims=True)
 
 
-A = np.array([[1, 2, 3], [3, 4, 5]])
+#
+def direction(w: List[float]) -> List[float]:
+    mag = sum([w_i ** 2 for w_i in w])
+    return [w_i / mag for w_i in w]
 
-print(A)
-np.testing.assert_array_equal(de_mean_matrix(A), np.array([[-1, -1, -1.], [ 1,  1,  1.]]))
+
+#
+def directional_variance_i(x_i, w):
+    """the variance of the row x_i in the direction determined by w"""
+    return
+
+#
+np.testing.assert_array_equal(de_mean_matrix(np.array([[1, 2, 3], [3, 4, 5]])), np.array([[-1, -1, -1.], [ 1,  1,  1.]]))
